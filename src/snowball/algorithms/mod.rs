bc58406d96c1b5be1a081f6b5edc3b54f3f40eee
Patrik Šimunič <patriksimunic@hotmail.com>
2024-05-03 03:17:34 +0200

    Add English (Lovins) algorithm

--- src/snowball/algorithms/mod.rs
+++ src/snowball/algorithms/mod.rs
@@ -4,6 +4,7 @@ pub mod czech_dolamic_aggressive;
 pub mod czech_dolamic_light;
 pub mod danish;
 pub mod dutch;
+pub mod english_lovins;
 pub mod english_porter;
 pub mod english_porter_2;
 pub mod finnish;
